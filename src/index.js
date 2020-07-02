83845abddd14324c769e0d472fad6ee1e7556b8c
Andrei <pavelhandup@gmail.com>
2020-07-02 11:09:41 +0200

    Refactored search and added App to its own folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import userData from './testData.json'
 
 ReactDOM.render(<App rows={userData} />, document.getElementById('root'));
